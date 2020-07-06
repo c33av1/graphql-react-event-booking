5fc04071799040cd29216a29f05b781acb5806b8
c33av1 <chhavi.negi4@gmail.com>
2020-07-06 16:44:59 -0700

    React - Added header and top nav

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,25 @@ import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import AuthPage from "./pages/Auth";
 import EventsPage from "./pages/Events";
 import BookingsPage from "./pages/Bookings";
+import MainNavigation from "./components/Navigation/MainNavigation";
 
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Redirect from="/" to="/auth" exact />
+      <React.Fragment>
+        <MainNavigation />
+        <main className="main-content">
+          <Switch>
+            <Redirect from="/" to="/auth" exact />
 
-        <Route path="/auth" component={AuthPage} />
-        <Route path="/events" component={EventsPage} />
-        <Route path="/bookings" component={BookingsPage} />
-      </Switch>
+            <Route path="/auth" component={AuthPage} />
+            <Route path="/events" component={EventsPage} />
+            <Route path="/bookings" component={BookingsPage} />
+          </Switch>
+        </main>
+      </React.Fragment>
     </BrowserRouter>
   );
 }
