1d1eb6580aa01010ff198dc72a60e1c7f491ab0a
c33av1 <chhavi.negi4@gmail.com>
2020-07-07 00:30:00 -0700

    React - Adding Modal to add event

---
+++ client/src/components/Modal/Modal.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./Modal.css";
+
+const Modal = ({ title, children, canCancel, canConfirm, onCancel }) => (
+  <div className="modal">
+    <header className="modal__header">
+      <h1>{title}</h1>
+    </header>
+    <section className="modal__content">{children}</section>
+    <section className="modal__actions">
+      {canCancel && (
+        <button className="btn" onClick={onCancel}>
+          Cancel
+        </button>
+      )}
+      {canConfirm && <button className="btn">Confirm</button>}
+    </section>
+  </div>
+);
+
+export default Modal;
--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -1,8 +1,50 @@
 import React, { Component } from "react";
 
+import Modal from "../components/Modal/Modal";
+import Backdrop from "../components/Backdrop/Backdrop";
+
+import "./Events.css";
+
 class AuthPage extends Component {
+  state = {
+    creating: false,
+  };
+
+  startCreateHandler = () => {
+    this.setState({ creating: true });
+  };
+
+  modalConfirmHandler = () => {
+    this.setState({ creating: false });
+  };
+
+  modalCancelHandler = () => {
+    this.setState({ creating: false });
+  };
+
   render() {
-    return <h1>the Events page</h1>;
+    return (
+      <React.Fragment>
+        {this.state.creating && <Backdrop />}
+        {this.state.creating && (
+          <Modal
+            title="Add Event"
+            canCancel
+            canConfirm
+            onCancel={this.modalCancelHandler}
+            onConfirm={this.modalConfirmHandler}
+          >
+            <p>Modal Content</p>
+          </Modal>
+        )}
+        <div className="events-control">
+          <p>Share your own Events!</p>
+          <button className="btn" onClick={this.startCreateHandler}>
+            Create Event
+          </button>
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
